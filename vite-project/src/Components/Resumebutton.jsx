356d4bb30a5043cf44290a7fda257b8a8e4f7632
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-03-20 20:38:41 +0200

    import resume pdf and document

--- vite-project/src/Components/Resumebutton.jsx
+++ vite-project/src/Components/Resumebutton.jsx
@@ -1,13 +1,18 @@
 import './resumebutton.css'
+import Resume from '../Assets/Resume.pdf';
+import { HiDownload } from "react-icons/hi";
 
 const Resumebutton = () =>  {
-      return (
-        <>
-        <button className="button resume-btn me-4 mr-4" type="button">
-          Resume
-        </button>
-        </>
-      );
+    return (
+      <>
+      <button className="button resume-btn me-4 mr-4 " type="button text-white " >
+        Resume
+      <a href={Resume} download className='btn primary icon-link  text-white '>
+       <HiDownload/>
+      </a> 
+      </button>
+      </>
+    );
     };
 
  export default Resumebutton;
